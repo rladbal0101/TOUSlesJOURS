6720c50c7ad65fdba1e8783d763a240d86a86d75
rladbal0101 <rladbal0101@naver.com>
2023-04-18 19:07:53 +0900

    TOUSlesJOURS

--- js/main.js
+++ js/main.js
@@ -20,3 +20,16 @@ btnCloseEl.addEventListener('click', function name(params) {
   searchInputEl.setAttribute('placeholder', '   제품명을 입력해 주세요.');
   btnCloseEl.classList.remove('focused');
 });
+
+// EVENT 수평 슬라이드 기능
+new Swiper('.event .swiper', {
+  direction: 'horizontal', // 수평 슬라이드(기본값)
+  loop: true, // 반복 재생 여부
+  autoplay: {
+    delay: 2000 // 5초마다 슬라이드 바뀜(기본값: 3000)
+  }, // 자동 재생 여부
+  pagination: { // 페이지 번호 사용
+    el: '.promotion .swiper-pagination', // 페이지 번호 요소 선택자
+    clickable: true // 사용자의 페이지 번호 요소 제어 가능 여부
+  },
+});
